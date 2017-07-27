126def4c86cfee23934c56b0bfa9d8c47d4737fc
Jack Huang <f26401004@gmail.com>
2017-07-27 13:31:09 +0800

    refresh mechanism, key listener, pic
    rewrite the refresh mechanism
    add key listener to move the fighter
    add the picture of fighter

--- src/mainScene.java
+++ src/mainScene.java
@@ -3,38 +3,139 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.TimerTask;
 import java.util.Timer;
+import java.awt.image.*;
+import javax.imageio.*;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import java.io.*;
 
-public class mainScene extends Canvas
+public class mainScene extends JComponent implements MouseListener, KeyListener
 {
 	// define the frame per second properties.
 	public static Byte frameCount = 0;
 	public static final Byte frameRate = 60;
+	public static final Double obstruct = 1.2;
 	
-	// define the angle and velocity.
-	private Double angle;
-	private Double velocity;
+	// define the X and Y variable.
+	private static Integer X;
+	private static Integer Y;
+	private static Double forceX;
+	private static Double forceY;
+	// define the bullet number.
+	private static Byte bulletNum;
 	// define the array list to store the bullet.
 	private ArrayList<Bullet> bulletList;
-	// initializer of mainScene
-	public mainScene()
-	{
-		// initialize the properties.
-		angle = 0.0;
-		velocity = 0.0;
-		bulletList = new ArrayList<Bullet>();
+	// define the fighter image
+	private static BufferedImage image;
+	// define the phase.
+	private Byte phase;
+	// constructor of mainScene
+    public mainScene(int x, int y)
+    {
+        super();
+        this.setLocation(x, y);
+        this.setSize(800, 640);
+        this.addKeyListener(this);
+        this.addMouseListener(this);
+        this.setFocusable(true);
+        this.requestFocusInWindow();
+        // initialize the properties.
+     	X = 0;
+     	Y = 0;
+     	forceX = 0.0;
+     	forceY = 0.0;
+    	bulletNum = 50;
+    	bulletList = new ArrayList<Bullet>();
+     	try {
+     		image = ImageIO.read(new File("src/Graphics/fighter.png"));
+        }
+        catch (Exception ex) {
+            System.out.println("No fighter.png!!");
+        }
+    }
+    // override paint function to draw the picture 
+    @Override
+    public void paintComponent(Graphics graphics)
+    {
+        super.paintComponent(graphics);
+        graphics.clearRect(0, 0, 800, 600);
+		// clear all the screen.
+		graphics.clearRect(0, 0, 800, 640);
+		graphics.drawImage(image, X, Y, null);
+        repaint();
+    }
+    // override key press function to move the fighter.
+    @Override
+    public void keyPressed(KeyEvent event) {
+		switch (event.getKeyCode())
+		{
+		case KeyEvent.VK_UP:
+			forceY = -20.0;
+			break;
+		case KeyEvent.VK_DOWN:
+			forceY = 20.0;
+			break;
+		case KeyEvent.VK_LEFT:
+			
+			forceX = -20.0;
+			break;
+		case KeyEvent.VK_RIGHT:
+			forceX = 20.0;
+			break;
+		}
+    }
+    
+	@Override
+	public void keyReleased(KeyEvent event) {
+		// TODO Auto-generated method stub
+
 	}
+	@Override
+	public void keyTyped(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	// main process
 	public static void main(String argv[])
 	{
-		// use AWT to draw the picture.
-		Frame frame  = new Frame("shoot");
-		frame.addWindowListener(new Adapter());
-		frame.setSize(800, 600);
-		// create a canvas.
-		mainScene canvas = new mainScene();
-		// add the canvas to the frame.
-		frame.add(canvas, BorderLayout.CENTER);
-		frame.setVisible(true);
+		// create a frame.
+        JFrame frame = new JFrame("fighter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+		// create the main scene
+		mainScene scene = new mainScene(0, 0);
+        // set the content of frame
+        frame.getContentPane().add(scene);
+        frame.pack();
+        frame.setSize(800, 640);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 		// create a timer task to set the refresh function
 		TimerTask refresh = new TimerTask() {
 		    public void run() {
@@ -42,9 +143,10 @@ public class mainScene extends Canvas
 				++frameCount;
 				if (frameCount > frameRate)
 					frameCount = 1;
+				// update the movement.
+				moveUpdate();
 				// repaint the screen.
-				frame.repaint();
-				canvas.repaint();
+				scene.repaint();
 		    }
 		};
 		// create a timer to count the FPS.
@@ -52,20 +154,18 @@ public class mainScene extends Canvas
 		// call the refresh function at fixed rate.
 		frameCounter.scheduleAtFixedRate(refresh, 1000 / frameRate, 1000 / frameRate);
 	}
-
-	// override paint function to draw the picture 
-	public void paint(Graphics graphics)
+	// use Newton's second law of motion to simulate the movement of fighter.
+	private static void moveUpdate()
 	{
-		graphics.clearRect(0, 0, 800, 600);
-		graphics.fillRect(10, 10, frameCount * 4, 10);
-	}
-}
-
-//create a class inherit the WindowAdapter to override some specific function.
-class Adapter extends WindowAdapter
-{
-	public void windowClosing(WindowEvent event)
-	{
-		System.exit(0);
+		X = X + (int)((forceX > 0 ? (forceX - obstruct) / 3 : (forceX + obstruct) / 3));
+		Y = Y + (int)((forceY > 0 ? (forceY - obstruct) / 3 : (forceY + obstruct) / 3));
+		if (forceX > 0)
+			forceX = Math.max(forceX - obstruct, 0.0);
+		else
+			forceX = Math.min(forceX + obstruct, 0.0);
+		if (forceY > 0)
+			forceY = Math.max(forceY - obstruct, 0.0);
+		else
+			forceY = Math.min(forceY + obstruct, 0.0);
 	}
 }
