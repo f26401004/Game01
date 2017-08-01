a8ef20a0f9ae6d6881a207a7a92ed98de63ce4ac
Jack Huang <f26401004@gmail.com>
2017-08-01 13:20:07 +0800

    add back, add bullet movement,
    add animate back
    add fire bullet movement mechanism
    add fire bullet effect
    add the restriction of firing bullet

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,36 +1,64 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.lang.Math;
+
+import javax.imageio.ImageIO;
 
 public class Bullet 
 {
-	public static final double G = 9.8;
-	public static double ForceCoeff = 0.2;
-	
-	private double X;
-	private double Y;
+	private int X;
+	private int Y;
 	private double velocityX;
 	private double velocityY;
-	
+	private BufferedImage image;
 	public Bullet()
 	{
 		X = 0;
 		Y = 0;
 		velocityX = 0;
 		velocityY = 0;
+     	try {
+     		image = ImageIO.read(new File("src/Graphics/bullet.png"));
+        }
+        catch (Exception ex) {
+            System.out.println("No bullet.png!!");
+        }
 	}
-	public Bullet(double VXValue, double VYValue)
+	public Bullet(int XValue, int YValue, double VXValue, double VYValue)
 	{
-		X = 0;
-		Y = 0;
+		X = XValue;
+		Y = YValue;
 		velocityX = VXValue;
 		velocityY = VYValue;
+     	try {
+     		image = ImageIO.read(new File("src/Graphics/bullet.png"));
+        }
+        catch (Exception ex) {
+            System.out.println("No bullet.png!!");
+        }
 	}
-	
 	public void refresh()
 	{
-		X = X + velocityX;
-		Y = Y + velocityY;
-		velocityX = velocityX - ForceCoeff * Math.pow(Math.hypot(velocityX, velocityY), 3) * velocityX;
-		velocityY = velocityY - G - ForceCoeff * Math.pow(Math.hypot(velocityX, velocityY), 3) * velocityY;
+		// refresh the properties.
+		X = X + (int)velocityX;
+		Y = Y + (int)velocityY;
+		// deal the obstruct effect.
+		if (velocityY > 0)
+			velocityY = Math.max(velocityY - 0.1, 0.0);
+		else
+			velocityY = Math.min(velocityY + 0.1, 0.0);
+	}
+	public int getX()
+	{
+		return X;
+	}
+	public int getY()
+	{
+		return Y;
+	}
+	public BufferedImage getImage()
+	{
+		return image;
 	}
 }
 
--- src/mainScene.java
+++ src/mainScene.java
@@ -15,18 +15,23 @@ public class mainScene extends JComponent implements MouseListener, KeyListener
 	public static Byte frameCount = 0;
 	public static final Byte frameRate = 60;
 	public static final Double obstruct = 1.2;
-	
 	// define the X and Y variable.
 	private static Integer X;
 	private static Integer Y;
 	private static Double forceX;
 	private static Double forceY;
+	private static boolean shoot;
 	// define the bullet number.
 	private static Byte bulletNum;
 	// define the array list to store the bullet.
-	private ArrayList<Bullet> bulletList;
+	private static ArrayList<Bullet> bulletList;
 	// define the fighter image
 	private static BufferedImage image;
+	// define the animate back picture.
+	private static Image aniBack;
+	// define the white back picture.
+	private static Image whiteBack;
+	private static float effectCounter;
 	// define the phase.
 	private Byte phase;
 	// constructor of mainScene
@@ -45,9 +50,13 @@ public class mainScene extends JComponent implements MouseListener, KeyListener
      	forceX = 0.0;
      	forceY = 0.0;
     	bulletNum = 50;
+    	shoot = false;
+    	effectCounter = 8;
     	bulletList = new ArrayList<Bullet>();
      	try {
      		image = ImageIO.read(new File("src/Graphics/fighter.png"));
+     		aniBack = Toolkit.getDefaultToolkit().createImage("src/Graphics/animateBack.gif");
+     		whiteBack = ImageIO.read(new File("src/Graphics/whiteBack.png"));
         }
         catch (Exception ex) {
             System.out.println("No fighter.png!!");
@@ -58,10 +67,34 @@ public class mainScene extends JComponent implements MouseListener, KeyListener
     public void paintComponent(Graphics graphics)
     {
         super.paintComponent(graphics);
-        graphics.clearRect(0, 0, 800, 600);
 		// clear all the screen.
 		graphics.clearRect(0, 0, 800, 640);
+		// draw animate back.
+		graphics.drawImage(aniBack, 0, 0, null);
+		// set the font and color.
+        graphics.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+        graphics.setColor(new Color(255, 255, 255));
 		graphics.drawImage(image, X, Y, null);
+		// draw the bullet.
+		for (Bullet iter: bulletList) 
+			graphics.drawImage(iter.getImage(), iter.getX(), iter.getY(), null);
+		// draw the rest number of bullet.
+		graphics.drawString("¤l¼u¼Æ¶q   " + Integer.toString(bulletNum), 650, 30);
+		// deal the shoot effect.
+		if (shoot && effectCounter > 0)
+		{
+			Graphics2D graphics2d = (Graphics2D) graphics;
+			// set the opacity.
+			graphics2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, effectCounter / 60));
+			effectCounter -= 0.8;
+			graphics2d.drawImage(whiteBack, 0, 0, null);
+			// if reach the end then turn off the flag and reset the properties.
+			if (effectCounter == 0)
+			{
+				effectCounter = 8;
+				shoot = false;
+			}
+		}
         repaint();
     }
     // override key press function to move the fighter.
@@ -70,18 +103,29 @@ public class mainScene extends JComponent implements MouseListener, KeyListener
 		switch (event.getKeyCode())
 		{
 		case KeyEvent.VK_UP:
-			forceY = -20.0;
+			if (Y > 20)
+				forceY = -20.0;
 			break;
 		case KeyEvent.VK_DOWN:
-			forceY = 20.0;
+			if (Y < 530)
+				forceY = 20.0;
 			break;
 		case KeyEvent.VK_LEFT:
-			
-			forceX = -20.0;
+			if (X > 20)
+				forceX = -20.0;
 			break;
 		case KeyEvent.VK_RIGHT:
-			forceX = 20.0;
+			if (X < 700)
+				forceX = 20.0;
 			break;
+		case KeyEvent.VK_SPACE:
+			if (bulletNum > 0)
+			{
+				bulletList.add(new Bullet(X + 20, Y + 12, 10.0, forceY / 5));
+				--bulletNum;
+				shoot = true;
+				effectCounter = 30;
+			}
 		}
     }
     
@@ -155,8 +199,10 @@ public class mainScene extends JComponent implements MouseListener, KeyListener
 		frameCounter.scheduleAtFixedRate(refresh, 1000 / frameRate, 1000 / frameRate);
 	}
 	// use Newton's second law of motion to simulate the movement of fighter.
+	// update the bullet movement.
 	private static void moveUpdate()
 	{
+		// update the movement of fighter
 		X = X + (int)((forceX > 0 ? (forceX - obstruct) / 3 : (forceX + obstruct) / 3));
 		Y = Y + (int)((forceY > 0 ? (forceY - obstruct) / 3 : (forceY + obstruct) / 3));
 		if (forceX > 0)
@@ -167,5 +213,13 @@ public class mainScene extends JComponent implements MouseListener, KeyListener
 			forceY = Math.max(forceY - obstruct, 0.0);
 		else
 			forceY = Math.min(forceY + obstruct, 0.0);
+		for (int i = 0; i < bulletList.size(); ++i)
+			bulletList.get(i).refresh();
+		// lock the bulletList and check the bullet reach the boundary.
+		synchronized (bulletList)
+		{
+			if (bulletList.size() > 0 && bulletList.get(0).getX() > 800)
+				bulletList.remove(0);
+		}
 	}
 }
